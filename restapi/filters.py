29381b90f98973a07591c61da145318e70aa82a9
AK-by <6306421@gmail.com>
2023-11-18 17:21:56 +0300

    Ordering & Search

--- restapi/filters.py
+++ restapi/filters.py
@@ -5,6 +5,27 @@ from . models import SimpleMessages
 class MessageFilter(filters.FilterSet):
     title = filters.CharFilter(field_name="title", lookup_expr="icontains")
     message = filters.CharFilter(field_name="message", lookup_expr="icontains")
+    created = filters.DateRangeFilter(field_name="created")
+    title_and_message = filters.CharFilter(method="filter_by_title_and_message")
+
+    # author = filters.ModelChoiceFilter(
+    # )
+    # author = filters.ModelMultipleChoiceFilter(
+    #     queryset=Author.objects.all(),
+    #     field_name="author",
+    #     to_field_name="id",
+    #     lookup_expr="exact",
+    # )
+
+    def filter_by_title_and_message(self, queryset, name, value):
+        parts = value.split(",")
+        title = parts[0]
+        message = parts[1]
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+        if message:
+            queryset = queryset.filter(message__icontains=message)
+        return queryset
 
     class Meta:
         model = SimpleMessages
--- restapi/views.py
+++ restapi/views.py
@@ -1,5 +1,5 @@
-from django_filters import rest_framework as filters
-from rest_framework import viewsets
+from django_filters import rest_framework as dj_filters
+from rest_framework import viewsets, filters
 from . serializers import SimpleMessagesSerializer
 from . models import SimpleMessages
 from . filters import MessageFilter
@@ -8,5 +8,9 @@ from . filters import MessageFilter
 class SimpleMessagesApiListView(viewsets.ModelViewSet):
     serializer_class = SimpleMessagesSerializer
     queryset = SimpleMessages.objects.all().order_by('pk')
-    filter_backends = (filters.DjangoFilterBackend, )
+    filter_backends = (dj_filters.DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter, )
     filterset_class = MessageFilter
+    ordering_fields = ("id", "title", "created", )
+    ordering = ("-created", )
+    search_fields = ("id", "title", "message", )
+    # search_fields = ("id", "title", "message", "author__name")  # Если через связь к таблице
